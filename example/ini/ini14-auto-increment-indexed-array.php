94a045d404cf594f18e97d82e0365a664f9d45d1
Ijor Tengab <m_roji28@yahoo.com>
2017-01-08 11:29:37 +0700

    penambahan informasi debug pada example ini 14

--- example/ini/ini14-auto-increment-indexed-array.php
+++ example/ini/ini14-auto-increment-indexed-array.php
@@ -60,3 +60,85 @@ $config->setData('[]', 'Whats Key Number? The Answer is 3')->save();
 // Lets see current value and content.
 var_dump($config->getData(), SUBJECT | COMMENT | LINE);
 var_dump((string) $config, SUBJECT | COMMENT | LINE);
+
+/**
+ * Line: 24
+ * var_dump($array):
+ * array(3) {
+ *     [0]=>
+ *     string(1) "0"
+ *     [1]=>
+ *     string(1) "1"
+ *     [2]=>
+ *     string(1) "2"
+ * }
+ */
+/**
+ * Line: 31
+ * var_dump($array):
+ * array(1) {
+ *     [0]=>
+ *     string(1) "0"
+ * }
+ */
+/**
+ * Line: 37
+ * var_dump($array):
+ * array(2) {
+ *     [0]=>
+ *     string(1) "0"
+ *     [3]=>
+ *     string(33) "Whats Key Number? The Answer is 3"
+ * }
+ */
+/**
+ * Line: 47
+ * var_dump($config->getData()):
+ * array(3) {
+ *     [0]=>
+ *     string(1) "0"
+ *     [1]=>
+ *     string(1) "1"
+ *     [2]=>
+ *     string(1) "2"
+ * }
+ */
+/**
+ * Line: 48
+ * var_dump((string) $config):
+ * string(27) "[] = '0'
+ * [] = '1'
+ * [] = '2'
+ * "
+ */
+/**
+ * Line: 54
+ * var_dump($config->getData()):
+ * array(1) {
+ *     [0]=>
+ *     string(1) "0"
+ * }
+ */
+/**
+ * Line: 55
+ * var_dump((string) $config):
+ * string(9) "[] = '0'
+ * "
+ */
+/**
+ * Line: 61
+ * var_dump($config->getData()):
+ * array(2) {
+ *     [0]=>
+ *     string(1) "0"
+ *     [3]=>
+ *     string(33) "Whats Key Number? The Answer is 3"
+ * }
+ */
+/**
+ * Line: 62
+ * var_dump((string) $config):
+ * string(46) "0 = '0'
+ * 3 = Whats Key Number? The Answer is 3
+ * "
+ */
